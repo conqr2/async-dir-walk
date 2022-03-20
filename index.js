7626705b6772259eb480045cb34515072d73f971
Conqr <34463817+conqr2@users.noreply.github.com>
2022-03-19 20:59:23 -0700

    push

--- index.js
+++ index.js
@@ -1,21 +1,23 @@
 const fs = require("fs");
 const path = require("path");
 
-const walk = dir =>
+const walk = (dir) =>
   new Promise((resolve, reject) => {
-    var results = [];
+    const results = [];
 
     fs.readdir(dir, (err, list) => {
       if (err) return reject(err);
       var pending = list.length;
       if (!pending) return resolve(results);
-      list.forEach(file => {
+      
+      list.forEach((file) => {
         file = path.resolve(dir, file);
+
         fs.stat(file, async (err, stat) => {
           if (err) reject(err);
+
           if (stat && stat.isDirectory()) {
             const res = await walk(file);
-
             results = results.concat(res);
             if (!--pending) resolve(results);
           } else {
